a3e4d8d12421e520481cfef1c2ad3325e62ca59b
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-28 13:30:54 +0100

    Added implementation of string repeat task solution

--- kyu-8/string-repeat/solution.js
+++ kyu-8/string-repeat/solution.js
@@ -1,7 +1,11 @@
-const sample = () => {
-  return 'dummy';
-}
+const repeatString = (n, s) => {
+  if (s === null || s === undefined) {
+    return s;
+  }
+  return Array.from(new Array(n), () => s)
+    .join('');
+};
 
 module.exports = {
-  sample
+  repeatString
 };
