c4f44a532e17f91b429d6451982d93287a28009a
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-28 19:56:25 +0100

    Added solution for "A Rule of Divisibility by 7" problem

--- kyu-7/divisibility-by-seven/solution-test.js
+++ kyu-7/divisibility-by-seven/solution-test.js
@@ -2,18 +2,18 @@ const solution = require('./solution');
 
 describe('seven', () => {
   it('should return same number and 0 steps for one digit number', () => {
-    expect(solution.seven(1)).toBe([1, 0]);
+    expect(solution.seven(1)).toEqual([1, 0]);
   });
   it('should return same number and 0 steps for two digit number', () => {
-    expect(solution.seven(12)).toBe([12, 0]);
+    expect(solution.seven(12)).toEqual([12, 0]);
   });
   it('should return correct result and 1 step for three digit number', () => {
-    expect(solution.seven(123)).toBe([6, 1]);
+    expect(solution.seven(123)).toEqual([6, 1]);
   });
   it('should return correct result and 2 steps for four digit number', () => {
-    expect(solution.seven(1234)).toBe([1, 2]);
+    expect(solution.seven(1234)).toEqual([1, 2]);
   });
   it('should return correct result and 3 steps for five digit number', () => {
-    expect(solution.seven(12345)).toBe([3, 3]);
+    expect(solution.seven(12345)).toEqual([3, 3]);
   });
 });
--- kyu-7/divisibility-by-seven/solution.js
+++ kyu-7/divisibility-by-seven/solution.js
@@ -1,5 +1,16 @@
 const seven = (m) => {
-  return 'dummy';
+  let step = 0;
+  while (m >= 100) {
+    m = iterate(m);
+    ++step;
+  }
+  return [m, step];
+}
+
+const iterate = (m) => {
+  const y = m % 10;
+  const x = (m - y) / 10;
+  return x - 2*y;
 }
 
 module.exports = {
