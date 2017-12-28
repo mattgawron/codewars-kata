f9642dfa2011bf0015cd74886351beef7812c532
Mateusz Gawron <bamboleho@gmail.com>
2017-12-28 19:42:57 +0100

    Added unit tests for "A Rule of Divisibility by 7" task

--- kyu-7/divisibility-by-seven/solution-test.js
+++ kyu-7/divisibility-by-seven/solution-test.js
@@ -1,7 +1,19 @@
 const solution = require('./solution');
 
-describe('sample', () => {
-  it('should return dummy string', () => {
-    expect(solution.sample()).toBe('dummy');
+describe('seven', () => {
+  it('should return same number and 0 steps for one digit number', () => {
+    expect(solution.seven(1)).toBe([1, 0]);
+  });
+  it('should return same number and 0 steps for two digit number', () => {
+    expect(solution.seven(12)).toBe([12, 0]);
+  });
+  it('should return correct result and 1 step for three digit number', () => {
+    expect(solution.seven(123)).toBe([6, 1]);
+  });
+  it('should return correct result and 2 steps for four digit number', () => {
+    expect(solution.seven(1234)).toBe([1, 2]);
+  });
+  it('should return correct result and 3 steps for five digit number', () => {
+    expect(solution.seven(12345)).toBe([3, 3]);
   });
 });
