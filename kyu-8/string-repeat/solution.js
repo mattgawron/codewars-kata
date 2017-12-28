227d19fa55b41e31060a032665924980daa81965
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-28 13:39:10 +0100

    Extracted method used to create array filled with elements

--- kyu-8/string-repeat/solution.js
+++ kyu-8/string-repeat/solution.js
@@ -1,9 +1,12 @@
+const createFilledArray = (size, elementProvider) => {
+  return Array.from(new Array(size), elementProvider);
+};
+
 const repeatString = (n, s) => {
   if (s === null || s === undefined) {
     return s;
   }
-  return Array.from(new Array(n), () => s)
-    .join('');
+  return createFilledArray(n, () => s).join('');
 };
 
 module.exports = {
