af33190a664803d53ed9686d5fad5e3a731c77df
Mateusz Gawron <bamboleho@gmail.com>
2017-12-28 18:05:24 +0100

    Added solution for two to one problem

--- kyu-7/two-to-one/solution.js
+++ kyu-7/two-to-one/solution.js
@@ -1,6 +1,21 @@
+const mapStringToLetters = (string) => string.split('');
+
+const filterUniqueElements = (array) => {
+  const reductor = (result, element) => {
+    if (!result.includes(element)) {
+      result.push(element);
+    }
+    return result;
+  };
+  return array.reduce(reductor, []);
+};
+
 const longest = (s1, s2) => {
-  return 'dummy';
-}
+  const firstStringLetters = mapStringToLetters(s1);
+  const secondStringLetters = mapStringToLetters(s2);
+  const allLetters = [...firstStringLetters, ...secondStringLetters];
+  return filterUniqueElements(allLetters).sort().join('');
+};
 
 module.exports = {
   longest
