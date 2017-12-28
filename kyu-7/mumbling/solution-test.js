622f7a74ab5bd6d0e3e684556e142999f0faa9f3
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-28 17:04:26 +0100

    Added unit test for mumbling problem

--- kyu-7/mumbling/solution-test.js
+++ kyu-7/mumbling/solution-test.js
@@ -1,7 +1,28 @@
 const solution = require('./solution');
 
-describe('sample', () => {
-  it('should return dummy string', () => {
-    expect(solution.sample()).toBe('dummy');
+describe('accum', () => {
+  it('should return empty string when empty string was provided', () => {
+    expect(solution.accum('')).toBe('');
+  });
+  it('should properly work for single small letter', () => {
+    expect(solution.accum('a')).toBe('A');
+  });
+  it('should properly work for single capital letter', () => {
+    expect(solution.accum('A')).toBe('A');
+  });
+  it('should properly work for multiple same small letters', () => {
+    expect(solution.accum('aaa')).toBe('A-Aa-Aaa');
+  });
+  it('should properly work for multiple same capital letters', () => {
+    expect(solution.accum('AAA')).toBe('A-Aa-Aaa');
+  });
+  it('should properly work for multiple different small letters', () => {
+    expect(solution.accum('abc')).toBe('A-Bb-Ccc');
+  });
+  it('should properly work for multiple different capital letters', () => {
+    expect(solution.accum('ABC')).toBe('A-Bb-Ccc');
+  });
+  it('should properly work for mix of small and capital letters', () => {
+    expect(solution.accum('aBcDeF')).toBe('A-Bb-Ccc-Dddd-Eeeee-Ffffff');
   });
 });
