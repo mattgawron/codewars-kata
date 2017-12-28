be033331ea3c37332c824cde865b23117e98f8f6
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-28 22:09:49 +0100

    Added solution for "Formatting decimal places #1"

--- kyu-7/formatting-decimal-places-no-1/solution.js
+++ kyu-7/formatting-decimal-places-no-1/solution.js
@@ -1,5 +1,9 @@
 const twoDecimalPlaces = (number) => {
-  return 'dummy';
+  const multipliedNumber = number * 100;
+  const multipliedInteger = number > 0
+    ? Math.floor(multipliedNumber)
+    : Math.ceil(multipliedNumber);
+  return multipliedInteger / 100;
 }
 
 module.exports = {
