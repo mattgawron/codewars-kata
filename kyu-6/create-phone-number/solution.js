400cda3fe380efb93e7c5a0b1184fe6c52d01077
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-29 23:11:06 +0100

    Added implementation for "Create Phone Number" task

--- kyu-6/create-phone-number/solution.js
+++ kyu-6/create-phone-number/solution.js
@@ -1,6 +1,15 @@
 const createPhoneNumber = (numbers) => {
-  return 'dummy';
+  const numberParts = [
+    joinSubarrayElements(numbers, 0, 3),
+    joinSubarrayElements(numbers, 3, 6),
+    joinSubarrayElements(numbers, 6, 10)
+  ];
+  return `(${numberParts[0]}) ${numberParts[1]}-${numberParts[2]}`;
 }
+
+const joinSubarrayElements = (array, begin, end) => {
+  return array.slice(begin, end).join('');
+};
 
 module.exports = {
   createPhoneNumber
