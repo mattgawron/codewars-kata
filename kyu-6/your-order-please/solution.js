d5c0c054c2db432d3e06b9a5029b523613a9ae44
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-29 22:58:47 +0100

    Added implementation for "Your order, please"

--- kyu-6/your-order-please/solution.js
+++ kyu-6/your-order-please/solution.js
@@ -1,6 +1,20 @@
 const order = (words) => {
-  return 'dummy';
+  if (!words.includes(' ')) {
+    return words;
+  }
+  return words.split(' ')
+    .map(mapWordToWordNumber)
+    .sort((first, second) => first.number - second.number)
+    .map((wordNumber) => wordNumber.word)
+    .join(' ');
 }
+
+const mapWordToWordNumber = (word) => {
+  return {
+    word,
+    number: +word.match(/\d+/g)
+  };
+};
 
 module.exports = {
   order
