d3ade3e8d0eb41a15f7c0fe0b244cf50e7e2192a
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-29 17:18:53 +0100

    Added solution for "The highest profit wins!" task

--- kyu-7/the-highest-profit-wins/solution.js
+++ kyu-7/the-highest-profit-wins/solution.js
@@ -1,6 +1,14 @@
-const minMax = () => {
-  return 'dummy';
+const minMax = (arr) => {
+  const initialResult = [arr[0], arr[0]];
+  return arr.reduce(updateMinMax, initialResult);
 }
+
+const updateMinMax = (result, value) => {
+  return [
+    value < result[0] ? value : result[0],
+    value > result[1] ? value : result[1] 
+  ];
+};
 
 module.exports = {
   minMax
