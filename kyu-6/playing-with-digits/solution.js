28933a0058ec77844e4bbdf66911951f5de5ea48
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-29 22:00:07 +0100

    Added implementation of "Playing with digits" task

--- kyu-6/playing-with-digits/solution.js
+++ kyu-6/playing-with-digits/solution.js
@@ -1,6 +1,14 @@
 const digPow = (n, p) => {
-  return 'dummy';
+  const digitPowersSum = n.toString()
+    .split('')
+    .map(digit => +digit)
+    .reduce(getDigitPowersSumReductor(p), 0);
+  return digitPowersSum % n === 0 ? digitPowersSum / n : -1;
 }
+
+const getDigitPowersSumReductor = (p) => {
+  return (result, digit, index) => result + Math.pow(digit, index + p);
+};
 
 module.exports = {
   digPow
