12b4f683d634b6d1c7a87adf2462d58c6a5beca8
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-29 17:35:05 +0100

    Added implementation for "shorter concat [reverse longer]"

--- kyu-7/shorter-concat-reverse-order/solution.js
+++ kyu-7/shorter-concat-reverse-order/solution.js
@@ -1,5 +1,11 @@
 const shorter_reverse_longer = (a, b) => {
-  return 'dummy';
+  const [shorter, longer] = b.length > a.length ? [a, b] : [b, a];
+  return shorter + reverse(longer) + shorter;
+}
+
+const reverse = (string) => {
+  return string.split('')
+    .reduce((result, symbol) => symbol + result, '');
 }
 
 module.exports = {
