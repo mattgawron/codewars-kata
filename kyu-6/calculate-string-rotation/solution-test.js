d6398a85c51d36e37223249d349b03818a57c443
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-30 19:52:36 +0100

    Updates unit tests for "Calculate string rotation" task

--- kyu-6/calculate-string-rotation/solution-test.js
+++ kyu-6/calculate-string-rotation/solution-test.js
@@ -2,15 +2,15 @@ const solution = require('./solution');
 
 describe('shiftedDiff', () => {
   it('should return 1 for pair: "esampl" and "sample"', () => {
-    expect(solution.shiftedDiff()).toBe();
+    expect(solution.shiftedDiff("esampl", "sample")).toBe(1);
   });
   it('should return 2 for pair: "stte" and "test"', () => {
-    expect(solution.shiftedDiff()).toBe();
+    expect(solution.shiftedDiff("stte", "test")).toBe(2);
   });
   it('should return 0 for pair: "same" and "same"', () => {
-    expect(solution.shiftedDiff()).toBe();
+    expect(solution.shiftedDiff("same", "same")).toBe(0);
   });
   it('should return -1 for pair: "not" and "rotated"', () => {
-    expect(solution.shiftedDiff()).toBe();
+    expect(solution.shiftedDiff("not", "rotated")).toBe(-1);
   });
 });
