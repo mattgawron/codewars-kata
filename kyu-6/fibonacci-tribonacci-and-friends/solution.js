c9f6706582e5e386f0c438267b97f62fdaa07a99
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-30 19:21:37 +0100

    Added solution to "Fibonacci, tribonacci and friends" task

--- kyu-6/fibonacci-tribonacci-and-friends/solution.js
+++ kyu-6/fibonacci-tribonacci-and-friends/solution.js
@@ -1,5 +1,16 @@
 const Xbonacci = (signature, n) => {
-  return 'dummy';
+  const signatureLength = signature.length;
+  const sequence = signature;
+
+  if (n <= signatureLength) {
+    return signature.slice(0, n);
+  }
+  for (let i = signatureLength; i < n; ++i) {
+    const nextElement = sequence.slice(i - signatureLength, i)
+      .reduce((sum, element) => sum + element, 0);
+    sequence.push(nextElement);
+  }
+  return sequence;
 }
 
 module.exports = {
