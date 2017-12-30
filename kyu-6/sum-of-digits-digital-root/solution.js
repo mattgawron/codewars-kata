e28b9a95e392e5a3d6c0c31c44d525a552c2d1b2
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-30 09:45:53 +0100

    Added solution for "Sum of Digits / Digital Root" task

--- kyu-6/sum-of-digits-digital-root/solution.js
+++ kyu-6/sum-of-digits-digital-root/solution.js
@@ -1,6 +1,17 @@
 const digital_root = (n) => {
-  return 'dummy';
+  let result = n;
+  while (result >= 10) {
+    result = getDigitSum(result);
+  }
+  return result;
 }
+
+const getDigitSum = (n) => {
+  return n.toString()
+    .split('')
+    .map(digit => +digit)
+    .reduce((sum, digit) => sum + digit, 0);
+};
 
 module.exports = {
   digital_root
