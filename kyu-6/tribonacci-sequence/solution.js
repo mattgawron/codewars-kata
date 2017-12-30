45f8ba04d218d22748ae9d29f6a9da123c8ca3a9
Mateusz Gawron <bamboleho+github@gmail.com>
2017-12-30 19:40:32 +0100

    Added solution for "Tribonacci sequence" task

--- kyu-6/tribonacci-sequence/solution.js
+++ kyu-6/tribonacci-sequence/solution.js
@@ -1,5 +1,16 @@
 const  tribonacci = (signature, n) => {
-  return 'dummy';
+  const signatureLength = signature.length;
+  const sequence = signature;
+
+  if (n <= signatureLength) {
+    return signature.slice(0, n);
+  }
+  for (let i = signatureLength; i < n; ++i) {
+    const nextElement = sequence.slice(i - signatureLength, i)
+      .reduce((sum, value) => sum + value, 0);
+    sequence.push(nextElement);
+  }
+  return sequence;
 }
 
 module.exports = {
