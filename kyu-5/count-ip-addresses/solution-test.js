2292b807d08bf05f095987ebdb53b0d996ddf57a
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-16 19:56:25 +0100

    Corrected position of parameters in unit tests

--- kyu-5/count-ip-addresses/solution-test.js
+++ kyu-5/count-ip-addresses/solution-test.js
@@ -5,15 +5,15 @@ describe('ipsBetween', () => {
     expect(solution.ipsBetween('10.0.0.0', '10.0.0.0')).toBe(1);
   });
   it('should work correctly when difference is in last field', () => {
-    expect(solution.ipsBetween('10.0.0.0', '10.0.0.20')).toBe(19);
+    expect(solution.ipsBetween('10.0.0.20', '10.0.0.0')).toBe(19);
   });
   it('should work correctly when difference is not in last field', () => {
-    expect(solution.ipsBetween('10.0.2.0', '10.0.0.0')).toBe(512);
+    expect(solution.ipsBetween('10.0.0.0', '10.0.2.0')).toBe(512);
   });
   it('should work correctly when there are multiple different fields', () => {
-    expect(solution.ipsBetween('10.0.4.10', '10.0.0.0')).toBe(1033);
+    expect(solution.ipsBetween('10.0.0.0', '10.0.4.10')).toBe(1033);
   });
   it('should work correctly when second field value is higher', () => {
-    expect(solution.ipsBetween('10.0.1.0', '10.0.0.100')).toBe(156);
+    expect(solution.ipsBetween('10.0.0.100', '10.0.1.0')).toBe(156);
   });
 });
