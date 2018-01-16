5e684345b592815f3ddf235e01cd64e947ae1d30
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-16 19:35:32 +0100

    Added solution for task "Convert string to camel case"

--- kyu-5/convert-string-to-camel-case/solution.js
+++ kyu-5/convert-string-to-camel-case/solution.js
@@ -1,5 +1,21 @@
+const wordMapper = (word, index) => {
+  if (word.length === 0) {
+    return '';
+  }
+  const isFirstWord = index === 0;
+  const firstLetter = word.substring(0, 1);
+  const otherLetters = word.substring(1, word.length);
+  return [
+    isFirstWord ? firstLetter : firstLetter.toUpperCase(),
+    otherLetters.toLowerCase()
+  ].join('');
+};
+
 const toCamelCase = (str) => {
-  return 'dummy';
+  const delimitersRegexp = /[_\-]/g;
+  return str.split(delimitersRegexp)
+    .map(wordMapper)
+    .join('');
 }
 
 module.exports = {
