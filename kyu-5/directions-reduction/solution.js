101c15bbe4f26accae004318bd538b5fb7529b56
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-16 19:12:44 +0100

    Added solution for task "Directions reduction"

--- kyu-5/directions-reduction/solution.js
+++ kyu-5/directions-reduction/solution.js
@@ -1,5 +1,26 @@
-const dirReduc = () => {
-  return 'dummy';
+const oppositeDirections = {
+  'NORTH': 'SOUTH',
+  'SOUTH': 'NORTH',
+  'WEST': 'EAST',
+  'EAST': 'WEST'
+};
+
+const directionsReductor = (reducedArray, direction, index, originalArray) => {
+  const oppositeDirection = oppositeDirections[direction];
+  const isPreviousOpposite = reducedArray.length > 0
+    ? reducedArray[reducedArray.length - 1] === oppositeDirection
+    : false;
+
+  if (isPreviousOpposite) {
+    reducedArray.pop();
+  } else {
+    reducedArray.push(direction);
+  }
+  return reducedArray;
+};
+
+const dirReduc = (arr) => {
+  return arr.reduce(directionsReductor, []);
 }
 
 module.exports = {
