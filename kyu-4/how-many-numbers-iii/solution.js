5d2c0792ab7b8f08892bb9f0366cf5c181d0c2d2
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-23 20:18:23 +0100

    Refactored code used to solve task "How many numbers III"

--- kyu-4/how-many-numbers-iii/solution.js
+++ kyu-4/how-many-numbers-iii/solution.js
@@ -1,3 +1,26 @@
+const cache = (() => {
+  const getKeyForCacheEntry = (n, k, min) => `${n},${k},${min}`;
+  const data = Array.from(new Array(10))
+    .map((_, index) => index + 1)
+    .reduce((cache, min) => cache[getKeyForCacheEntry(0, 0, min)] = [''], {});
+
+  return {
+    isInCache: function(n, k, min) {
+      return data.hasOwnProperty(getKeyForCacheEntry(n, k, min));
+    },
+    putToCache: function(n, k, min, numbers) {
+      if (!this.isInCache(n, k, min)) {
+        data[getKeyForCacheEntry(n, k, min)] = numbers;
+      }
+    },
+    getFromCache: function(n, k, min) {
+      return this.isInCache(n, k, min)
+        ? data[getKeyForCacheEntry(n, k, min)]
+        : [];
+    }
+  }
+})();
+
 const findAll = (n, k) => {
   const numbers = generateNumbers(n, k, 1);
   return numbers.length > 0
@@ -9,8 +32,8 @@ const generateNumbers = (n, k, min) => {
   if (n <= 0 || k <= 0) {
     return [''];
   }
-  if (isInCache(n, k, min)) {
-    return getFromCache(n, k, min);
+  if (cache.isInCache(n, k, min)) {
+    return cache.getFromCache(n, k, min);
   }
   let numbers = [];
   for (let nextNumber = min; nextNumber <= Math.min(n/k, 10); ++nextNumber) {
@@ -26,35 +49,9 @@ const generateNumbers = (n, k, min) => {
       ];
     }
   }
-  putToCache(n, k, min, numbers);
+  cache.putToCache(n, k, min, numbers);
   return numbers;
 };
-
-const cache = {};
-
-// const initCache = () => {
-//   const cache = {};
-//   for (let i = 1; i < 10; ++i) {
-//     cache[getKeyForCacheEntry(0, 0, i)] = [''];
-//   }
-//   return cache;
-// };
-
-const isInCache = (n, k, min) => {
-  return cache.hasOwnProperty(getKeyForCacheEntry(n, k, min));
-};
-
-const putToCache = (n, k, min, numbers) => {
-  if (!isInCache(n, k, min)) {
-    cache[getKeyForCacheEntry(n, k, min)] = numbers;
-  }
-};
-
-const getFromCache = (n, k, min) => {
-  return isInCache(n, k, min) ? cache[getKeyForCacheEntry(n, k, min)] : [];
-};
-
-const getKeyForCacheEntry = (n, k, min) => `${n},${k},${min}`;
 
 module.exports = {
   findAll
