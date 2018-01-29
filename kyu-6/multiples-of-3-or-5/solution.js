11d9cb7f75e3e779aa8bce37e618a5f3bf70bfc4
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-29 19:46:41 +0100

    Added solution to task "Multiples of 3 or 5"

--- kyu-6/multiples-of-3-or-5/solution.js
+++ kyu-6/multiples-of-3-or-5/solution.js
@@ -1,6 +1,14 @@
 const solution = (number) => {
-  return 'dummy';
+  const multiplesByThree = multiplesBy(number, 3);
+  const multiplesByFive = multiplesBy(number, 5);
+  const multiplesByFifteen = multiplesBy(number, 15);
+  return multiplesByThree + multiplesByFive - multiplesByFifteen;
 }
+
+const multiplesBy = (number, divider) => {
+  const greatestMultiplier = Math.floor((number - 1) / divider);
+  return divider * greatestMultiplier * (greatestMultiplier + 1) / 2;
+};
 
 module.exports = {
   solution
