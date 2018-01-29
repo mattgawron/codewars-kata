b203c066eeaaf67434c49c921757b2a733936cb4
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-29 20:01:56 +0100

    Added unit tests for "Integer depth" task

--- kyu-6/integer-depth/solution-test.js
+++ kyu-6/integer-depth/solution-test.js
@@ -1,7 +1,19 @@
 const solution = require('./solution');
 
-describe('sample', () => {
-  it('should return dummy string', () => {
-    expect(solution.sample()).toBe('dummy');
+describe('computeDepth', () => {
+  it('should return 10 for x = 1', () => {
+    expect(solution.computeDepth(1)).toBe(10);
+  });589
+  it('should return 9 for x = 42', () => {
+    expect(solution.computeDepth(42)).toBe(9);
+  });
+  it('should return 13 for x = 12', () => {
+    expect(solution.computeDepth(12)).toBe(13);
+  });
+  it('should return 9 for x = 21', () => {
+    expect(solution.computeDepth(21)).toBe(9);
+  });
+  it('should return 9 for x = 123', () => {
+    expect(solution.computeDepth(123).toBe(9);
   });
 });
