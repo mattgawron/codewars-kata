487d4b34c6e20342e49bad7c46303ab12c34fec9
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-29 19:45:30 +0100

    Corrected unit tests for "Multiples of 3 or 5"

--- kyu-6/multiples-of-3-or-5/solution-test.js
+++ kyu-6/multiples-of-3-or-5/solution-test.js
@@ -5,18 +5,18 @@ describe('solution', () => {
     expect(solution.solution(0)).toBe(0);
   });
   it('should return 0 for number = 3', () => {
-    expect(solution.solution(0)).toBe(0);
+    expect(solution.solution(3)).toBe(0);
   });
   it('should return 3 for number = 4', () => {
-    expect(solution.solution(3)).toBe(3);
+    expect(solution.solution(4)).toBe(3);
   });
   it('should return 3 for number = 5', () => {
-    expect(solution.solution(3)).toBe(3);
+    expect(solution.solution(5)).toBe(3);
   });
   it('should return 8 for number = 6', () => {
-    expect(solution.solution(8)).toBe(8);
+    expect(solution.solution(6)).toBe(8);
   });
   it('should return 23 for number = 10', () => {
-    expect(solution.solution(23)).toBe(23);
+    expect(solution.solution(10)).toBe(23);
   });
 });
