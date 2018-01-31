7a0ef843f96239a9f1191bae24224483fcf07fd3
Mateusz Gawron <bamboleho+github@gmail.com>
2018-01-31 19:28:39 +0100

    Added initial solution

--- kyu-6/reverse-or-rotate/solution.js
+++ kyu-6/reverse-or-rotate/solution.js
@@ -1,5 +1,24 @@
 const revrot = (str, sz) => {
-  return 'dummy';
+  if (str.length === 0 || sz <= 0) {
+    return '';
+  }
+  const chunkRegexp = new RegExp(`[0-9]{1,${sz}}`, 'g');
+  return str.match(chunkRegexp)
+    .filter(chunk => chunk.length === sz)
+    .map(chunk => chunk.split(''))
+    .map(splittedChunk => splittedChunk.map(digit => +digit))
+    .map(splittedChunk => {
+      const cubesSum = splittedChunk.map(digit => Math.pow(digit, 3))
+        .reduce((sum, cube) => sum + cube, 0);
+      if (cubesSum % 2 === 0) {
+        return splittedChunk.reverse();
+      } else {
+        const firstDigit = splittedChunk.shift();
+        return [...splittedChunk, firstDigit];
+      }
+    })
+    .map(splittedChunk => splittedChunk.join(''))
+    .join('');
 }
 
 module.exports = {
