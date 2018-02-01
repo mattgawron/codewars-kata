0e8d677bbf87cb1ed227fa614008181b423b1008
Mateusz Gawron <bamboleho+github@gmail.com>
2018-02-01 21:28:47 +0100

    Added solution to task "Non-even substrings"

--- kyu-6/non-even-substrings/solution.js
+++ kyu-6/non-even-substrings/solution.js
@@ -1,6 +1,12 @@
 const solve = (s) => {
-  return 'dummy';
-}
+  return s.split('')
+    .reduce((sum, element, index) => {
+      if (element % 2 === 1) {
+        sum += index + 1;
+      }
+      return sum;
+    }, 0);
+};
 
 module.exports = {
   solve
