1139260741898703c860fa8069f17c964f5917c9
Mateusz Gawron <bamboleho+github@gmail.com>
2018-02-01 21:03:57 +0100

    Added solution to task "Decipher this"

--- kyu-6/decipher-this/solution.js
+++ kyu-6/decipher-this/solution.js
@@ -1,6 +1,26 @@
 const decipherThis = (str) => {
-  return 'dummy';
-}
+  return str.split(' ')
+    .map(decodeSingleWord)
+    .join(' ');
+};
+
+const decodeSingleWord = (word) => {
+  const encodedRegexp = /(\d+)(\w+)/;
+  const [original, firstLetterCode, otherChars] = word.match(encodedRegexp);
+  const firstLetter = String.fromCharCode(+firstLetterCode);
+  const decryptedOtherChars = swapFirstAndLastCharacter(otherChars);
+  return firstLetter + decryptedOtherChars;
+};
+
+const swapFirstAndLastCharacter = (word) => {
+  if (word.length < 2) {
+    return word;
+  }
+  const firstCharacter = word[0];
+  const middle = word.slice(1, word.length - 1);
+  const lastCharacter = word[word.length - 1];
+  return lastCharacter + middle + firstCharacter;
+};
 
 module.exports = {
   decipherThis
